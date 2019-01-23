99e99f1c1d66c649da43d0c1f27f9d4841fcc197
Vincent Riemer <vincentriemer@gmail.com>
2019-01-22 22:18:57 -0500

    tweak unstable warning

--- scripts/unstableWarning.js
+++ scripts/unstableWarning.js
@@ -5,8 +5,16 @@ import { ROOT_DIR } from "./paths";
 const maybeScopedDir = path.resolve(ROOT_DIR, "..").split(path.sep);
 const maybeScopedDirname = maybeScopedDir[maybeScopedDir.length - 1];
 
-if (maybeScopedDirname === "@unstable") {
+var nex = process.env.npm_execpath;
+
+if (nex && maybeScopedDirname === "@unstable") {
+  const isNpm = nex.includes("npm");
+
+  const prefix = isNpm
+    ? `npm \u001b[43m\u001b[30mWARN\u001b[0m \x1b[35munstable\u001b[0m`
+    : `\x1b[33mwarning\u001b[0m`;
+
   console.warn(
-    `\u001b[33mwarning\u001b[0m @unstable/addimated is an experimental package, continue at your own risk`
+    `${prefix} @unstable/addimated is an experimental package, continue at your own risk`
   );
 }
